4ee5e481329578d9d16b5d5522b3ebd042c1c9b4
Hassan Muhammad Yousuf <alih3766@gmail.com>
2024-03-20 04:43:22 +0100

    defining funtions in Instructor and MeetingTime Classes

--- main.py
+++ main.py
@@ -23,11 +23,26 @@ class Course:
     def __str__(self):
         return self._name
 class Instructor:
-    ''' '''
+    def __init__(self, id, name):
+        self._id = id
+        self._name = name
+    def get_id(self):
+        return self._id
+    def get_name(self):
+        return self._name
+    def __str__(self):
+        return self._name
+    
 class Room:
     ''' '''
 class MeetingTime:
-    ''' '''
+    def __init__(self, id, time):
+        self._id = id
+        self._time = time
+    def get_id(self):
+        return self._id
+    def get_time(self):
+        return self._time
 class Department:
     def __init__(self, name, courses):
         self._name = name
